637938a2229f73f330b1ddfb859590b64ffcb2a6
王兴岭 <wxlhdm@qq.com>
2017-02-13 17:30:57 +0800

    Signed-off-by: 王兴岭 <wxlhdm@qq.com>

--- Lesson1/app/src/main/java/com/lemo/com/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/com/lemo/com/lesson1/MainActivity.java
@@ -2,12 +2,36 @@ package com.lemo.com.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button trueBtn;
+    private Button falseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trueBtn = (Button) findViewById(R.id.btn_true);
+        falseBtn = (Button) findViewById(R.id.btn_false);
+
+        trueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,R.string.correct,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        falseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,R.string.incorrect,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
