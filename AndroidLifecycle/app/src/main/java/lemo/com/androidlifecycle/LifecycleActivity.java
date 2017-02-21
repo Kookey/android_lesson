17ee43adeeb2725c55776c6523dc19ff110fde54
王兴岭 <wxlhdm@qq.com>
2017-02-21 12:57:07 +0800

    Signed-off-by: 王兴岭 <wxlhdm@qq.com>

---
+++ AndroidLifecycle/app/src/main/java/lemo/com/androidlifecycle/LifecycleActivity.java
@@ -0,0 +1,46 @@
+package lemo.com.androidlifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LifecycleActivity extends AppCompatActivity {
+
+    private static final String TAG = "LifecycleActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lifecycle);
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+}
